107769e896ba76787665cbe376da778fcf07dcc7
Marcos <marcosvgb13.mb@gmail.com>
2023-07-10 23:08:08 -0300

    second commit

---
+++ src/main/java/com/example/Loja/repository/lojaRepository.java
@@ -0,0 +1,11 @@
+//package com.example.Loja.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.example.Loja.model.Marca;
+//
+//public interface lojaRepository extends JpaRepository<Marca, Long>{
+//
+//	
+//	
+//}
